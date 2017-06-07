86772c612a7b0611bf83eba1763997062627040d
Arnob Alam <arnoblalam@gmail.com>
2017-06-07 07:16:44 -0400

    Fix list filter

--- src/assets/js/single-page/list.js
+++ src/assets/js/single-page/list.js
@@ -6,35 +6,40 @@ var codeToc = new List('code-toc', options);
 
 codeToc.sort('chaps', {alphabet: '2345678901'})
 
+var myfilter = function (item) {
+    active_lang_filter = $('.langfilter.success').attr('id')
+    active_chap_filter = $('.chapfilter.success').attr('id')
+    lang = item.values().lang.split(",")
+    chaps = item.values().chaps.split(",")
+    if (active_lang_filter == null && active_chap_filter == null) {
+        return true
+    }
+    else if(active_lang_filter && active_chap_filter) {
+        return ($.inArray(active_lang_filter, lang) !== -1) && ($.inArray(active_chap_filter, chaps) !== -1);
+    }
+    else if (active_lang_filter) {
+        return ($.inArray(active_lang_filter, lang) !== -1)
+    }
+    else if (active_chap_filter) {
+        return $.inArray(active_chap_filter, chaps) !== -1
+    }
+}
+
 $('.langfilter').click(function() {
-    myId = $(this).attr('id')
     currentlyActive = ($(this)).hasClass('success')
     $('.langfilter').removeClass('success')
-    if(currentlyActive) {
-		codeToc.filter()
+    if(!currentlyActive) {
+        $(this).addClass('success')
 	}
-	else {
-		$(this).addClass('success')
-		codeToc.filter(function(item) {
-			chaps = item.values().lang.split(",")
-            console.log(myId)
-			return $.inArray(myId, chaps) !== -1;
-		})
-	}})
+    codeToc.filter(myfilter)
+})
 
 
 $('.chapfilter').click(function() {
-	myId = $(this).attr('id')
 	currentlyActive = ($(this)).hasClass('success')
 	$('.chapfilter').removeClass('success')
-	if(currentlyActive) {
-		codeToc.filter()
+	if(!currentlyActive) {
+        $(this).addClass('success')
 	}
-	else {
-		$(this).addClass('success')
-		codeToc.filter(function(item) {
-			chaps = item.values().chaps.split(",")
-			return $.inArray(myId, chaps) !== -1;
-		})
-	}
+    codeToc.filter(myfilter)
 })
