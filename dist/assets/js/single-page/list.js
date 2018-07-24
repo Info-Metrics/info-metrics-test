cc2a3b8b3f34b0ad9d0c3aa7c2a0436ed4ee314d
arnoblalam@gmail.com <arnoblalam@gmail.com>
2018-07-24 12:30:13 -0400

    COmmit all

--- dist/assets/js/single-page/list.js
+++ dist/assets/js/single-page/list.js
@@ -1,1 +1,45 @@
-var options={valueNames:["name","lang","keys","chaps"]},codeToc=new List("code-toc",options);codeToc.sort("chaps",{alphabet:"2345678901"});var myfilter=function(e){return active_lang_filter=$(".langfilter.success").attr("id"),active_chap_filter=$(".chapfilter.success").attr("id"),lang=e.values().lang.split(","),chaps=e.values().chaps.split(","),null==active_lang_filter&&null==active_chap_filter||(active_lang_filter&&active_chap_filter?$.inArray(active_lang_filter,lang)!==-1&&$.inArray(active_chap_filter,chaps)!==-1:active_lang_filter?$.inArray(active_lang_filter,lang)!==-1:active_chap_filter?$.inArray(active_chap_filter,chaps)!==-1:void 0)};$(".langfilter").click(function(){currentlyActive=$(this).hasClass("success"),$(".langfilter").removeClass("success"),currentlyActive||$(this).addClass("success"),codeToc.filter(myfilter)}),$(".chapfilter").click(function(){currentlyActive=$(this).hasClass("success"),$(".chapfilter").removeClass("success"),currentlyActive||$(this).addClass("success"),codeToc.filter(myfilter)});
+var options = {
+  valueNames: [ 'name', 'lang', 'keys', 'chaps' ]
+};
+
+var codeToc = new List('code-toc', options);
+
+codeToc.sort('chaps', {alphabet: '2345678901'})
+
+var myfilter = function (item) {
+    active_lang_filter = $('.langfilter.success').attr('id')
+    active_chap_filter = $('.chapfilter.success').attr('id')
+    lang = item.values().lang.split(",")
+    chaps = item.values().chaps.split(",")
+    if (active_lang_filter == null && active_chap_filter == null) {
+        return true
+    }
+    else if(active_lang_filter && active_chap_filter) {
+        return ($.inArray(active_lang_filter, lang) !== -1) && ($.inArray(active_chap_filter, chaps) !== -1);
+    }
+    else if (active_lang_filter) {
+        return ($.inArray(active_lang_filter, lang) !== -1)
+    }
+    else if (active_chap_filter) {
+        return $.inArray(active_chap_filter, chaps) !== -1
+    }
+}
+
+$('.langfilter').click(function() {
+    currentlyActive = ($(this)).hasClass('success')
+    $('.langfilter').removeClass('success')
+    if(!currentlyActive) {
+        $(this).addClass('success')
+	}
+    codeToc.filter(myfilter)
+})
+
+
+$('.chapfilter').click(function() {
+	currentlyActive = ($(this)).hasClass('success')
+	$('.chapfilter').removeClass('success')
+	if(!currentlyActive) {
+        $(this).addClass('success')
+	}
+    codeToc.filter(myfilter)
+})
